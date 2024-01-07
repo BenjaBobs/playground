ff6f52477022c647ec9c10598866fc1563f6e501
BenjaBobs <behub92@gmail.com>
2024-01-07 15:15:52 +0100

    Set base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/playground/",
   resolve: {
     alias: [{ find: "@src", replacement: "/src" }],
   },
