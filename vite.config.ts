76534135df28898e5ff3d29b761d46a0af860fbc
Benjamin <behub92@gmail.com>
2024-05-23 23:10:20 +0200

    Switch out SWC react plugin for babel react plugin to make compatible with react-beyond

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+// import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { checker } from "vite-plugin-checker";
+import reactBeyond from "react-beyond/plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,10 +11,11 @@ export default defineConfig({
     alias: [{ find: "@src", replacement: "/src" }],
   },
   plugins: [
-    react(),
     checker({
       // e.g. use TypeScript check
       typescript: true,
     }),
+    react(),
+    reactBeyond(),
   ],
 });
